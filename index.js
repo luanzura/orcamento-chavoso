408ee0ba9ac208a0b944a79f003ef0516fef4298
Luanzura <luanmaximus@gmail.com>
2024-05-21 13:52:00 -0300

    add sistema que cria uma img da div

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-$(document).ready(function(){
-  $('#gerar').on('click', function() {
-    const quantidade = parseFloat($("#quantidade").val());
-    const valorTotal = parseFloat($("#valorTotal").val());
-    const desconto = parseFloat($("#desconto").val());
-    const frete = parseFloat($("#frete").val());
-    const freteTipo = $('input[type="radio"]:checked').val();
-    const totalSemDesconto = valorTotal + frete;
-    const totalComDesconto = valorTotal - desconto + frete;
+$(document).ready(function() {
+    $('#gerar').on('click', function() {
+        const quantidade = parseFloat($("#quantidade").val());
+        const valorTotal = parseFloat($("#valorTotal").val());
+        const desconto = parseFloat($("#desconto").val());
+        const frete = parseFloat($("#frete").val());
+        const freteTipo = $('input[type="radio"]:checked').val();
+        const totalSemDesconto = valorTotal + frete;
+        const totalComDesconto = valorTotal - desconto + frete;
 
-    console.log("Quantidade: " + quantidade);
-    console.log("Valor Total: " + valorTotal);
-    console.log("Desconto: " + desconto);
-    console.log("Frete: " + frete);
-    console.log("Tipo de Frete: " + freteTipo);
+        $(".quantidadeResultado").text(quantidade);
+        $(".valorResultado").text("R$" + valorTotal.toFixed(2));
+        $(".freteValorResultado").text("R$" + frete.toFixed(2));
+        $(".freteTipoResultado").text(freteTipo);
+        $(".descontoResultado").text("R$" + desconto.toFixed(2));
+        $(".totalSemResultado").text("R$" + totalSemDesconto.toFixed(2));
+        $(".totalComResultado").text("R$" + totalComDesconto.toFixed(2));
+    });
 
-    $(".quantidadeResultado").text(quantidade);
-    $(".valorResultado").text("R$" + valorTotal.toFixed(2)); 
-    $(".freteValorResultado").text("R$" + frete.toFixed(2));
-    $(".freteTipoResultado").text(freteTipo);
-    $(".descontoResultado").text("R$" + desconto.toFixed(2));
-    $(".totalSemResultado").text("R$" + totalSemDesconto.toFixed(2));
-    $(".totalComResultado").text("R$" + totalComDesconto.toFixed(2));
-    
-    
-  });
+    $('#gerar').on('click', function() {
+        html2canvas(document.querySelector('.gerar')).then(canvas => {
+            canvas.toBlob(function(blob) {
+                const item = new ClipboardItem({ 'image/png': blob });
+                navigator.clipboard.write([item]).then(function() {
+                    alert('Imagem copiada!');
+                }).catch(function(error) {
+                    console.error('Erro: ', error);
+                });
+            });
+        });
+    });
 });
