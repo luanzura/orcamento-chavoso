e41282b5ade45b1868974dfd68f4aad3a3d9c53b
Luanzura <luanmaximus@gmail.com>
2024-05-31 12:32:44 -0300

    buscaCep.js agora usa jQuery

--- buscaCep.js
+++ buscaCep.js
@@ -1,29 +1,23 @@
 function buscaCep() {
-  let reCep = document.getElementById("cep").value;
+  let reCep = $("#cep").val();
   if (reCep !== "") {
     let url = "https://viacep.com.br/ws/" + reCep + "/json/";
 
-    let req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.send();
-
-    req.onload = function () {
-      if (req.status === 200) {
-        let endereco = JSON.parse(req.response);
-        document.getElementById("rua").value = endereco.logradouro;
-        document.getElementById("cidade").value = endereco.localidade;
-        document.getElementById("estado").value = endereco.uf;
-        document.getElementById("bairro").value = endereco.bairro;
-      } else if (req.status === 400) {
+    $.get(url, function (endereco) {
+      if (endereco.erro) {
         alert("Cep inválido");
       } else {
-        alert("Erro ao fazer a requisição");
+        $("#rua").val(endereco.logradouro);
+        $("#cidade").val(endereco.localidade);
+        $("#estado").val(endereco.uf);
+        $("#bairro").val(endereco.bairro);
       }
-    };
+    }).fail(function () {
+      alert("Erro ao fazer a requisição");
+    });
   }
 }
 
-window.onload = function () {
-  let txtReCep = document.getElementById("cep");
-  txtReCep.addEventListener("input", buscaCep);
-};
+$(document).ready(function () {
+  $("#cep").on("input", buscaCep);
+});
